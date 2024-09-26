cfc3d50ef5c5f59edccbbeb781fc909272d0a6f3
gambitier <akash.jadhav.cse@gmail.com>
2024-09-26 22:00:37 +0530

    chore: remove redundant column from db table  QuestionAssignment

---
+++ prisma/migrations/20240926162859_remove_col_from_question_assignment/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `cycleDuration` on the `QuestionAssignment` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "QuestionAssignment" DROP COLUMN "cycleDuration";
