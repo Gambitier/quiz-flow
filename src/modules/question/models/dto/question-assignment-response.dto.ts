8fb0a1e02727ee066a526a699af931ea1757b090
gambitier <akash.jadhav.cse@gmail.com>
2024-09-27 16:15:36 +0530

    fix: remove unused props

--- src/modules/question/models/dto/question-assignment-response.dto.ts
+++ src/modules/question/models/dto/question-assignment-response.dto.ts
@@ -2,8 +2,6 @@ export class QuestionAssignmentResponseDto {
   id: string;
   regionId: string;
   questionId: string;
-  scheduledStartAt?: Date;
-  scheduledEndAt?: Date;
 
   constructor(partial: Partial<QuestionAssignmentResponseDto>) {
     Object.assign(this, partial);
