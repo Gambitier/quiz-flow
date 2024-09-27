6e04ac8d8ebca1061f65c9704807855e71de0319
gambitier <akash.jadhav.cse@gmail.com>
2024-09-27 20:32:31 +0530

    fix: add ApiProperty annotation

--- src/modules/regional-question-cycle/models/request/add-new-cycle.request.ts
+++ src/modules/regional-question-cycle/models/request/add-new-cycle.request.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { IsDate, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class AddNewCycleRequest {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @IsUUID()
   regionId: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
   cycleStart: Date;
 
+  @ApiProperty()
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
