09796871a2739250d1ec8603d502028541d345fc
gambitier <akash.jadhav.cse@gmail.com>
2024-09-27 17:03:22 +0530

    chore: regenerate migrations

--- prisma/migrations/20240926204455_minor_fix_to_regional_question_cycle/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "RegionalQuestionCycle" DROP CONSTRAINT "RegionalQuestionCycle_questionAssignmentId_fkey";
-
--- AlterTable
-ALTER TABLE "RegionalQuestionCycle" ALTER COLUMN "questionAssignmentId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "RegionalQuestionCycle" ADD CONSTRAINT "RegionalQuestionCycle_questionAssignmentId_fkey" FOREIGN KEY ("questionAssignmentId") REFERENCES "QuestionAssignment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
